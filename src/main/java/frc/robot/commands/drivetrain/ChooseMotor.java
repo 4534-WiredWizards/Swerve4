9c2e0bf3aa1f6a85571cf592f8e88d3f2cdcf866
WWLaptop <WWgithub@wiredwizards.org>
2021-10-25 19:19:09 -0400

    Created a MotorChooser
    Created SwerveModule constructors

--- src/main/java/frc/robot/commands/drivetrain/ChooseMotor.java
+++ src/main/java/frc/robot/commands/drivetrain/ChooseMotor.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SwerveModule;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -10,27 +11,22 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 //Assign each motor to a button
 
 public class ChooseMotor extends CommandBase {
+
     
-    private final Command flDrive =  new FrontLeftDrive();
-    private final Command flRotation = new FrontLeftRotation();
-    private final Command frDrive = new FrontRightDrive();
-    private final Command frRotation = new FrontRightRotation();
-    private final Command rlDrive = new RearLeftDrive();
-    private final Command rlRotation = new RearLeftRotation();
-    private final Command rrDrive = new RearRightDrive();
-    private final Command rrRotation = new RearRightRotation();
+    private final Command frontLeft =  new SwerveModule(frontRightDriveMotorId, frontRightRotationMotorId, );
+    private final Command frontRight = new SwerveModule();
+    private final Command backLeft = new SwerveModule();
+    private final Command backRight = new SwerveModule();
+    // Create instances of Swerve Module and pass in difference drive and rotation motor IDs. 
+
     public SendableChooser<Command> motorChooser = new SendableChooser<Command>();
 
     public ChooseMotor() {
         
-        motorChooser.setDefaultOption("frontleftDrive", flDrive);
-        motorChooser.addOption("frontleftRotation", flRotation);
-        motorChooser.addOption("frontrightDrive", frDrive);
-        motorChooser.addOption("frontrightRotation", frRotation);
-        motorChooser.addOption("rearleftDrive", rlDrive);
-        motorChooser.addOption("rearleftRotation", rlRotation);
-        motorChooser.addOption("rearrightDrive", rrDrive);
-        motorChooser.addOption("rearrightRotation", rrRotation);
+        motorChooser.addOption("frontLeft", frontLeft);
+        motorChooser.addOption("frontRight", frontRight);
+        motorChooser.addOption("backLeft", backRight);
+        motorChooser.addOption("backRight", backLeft);
         SmartDashboard.putData(motorChooser);
 
 
